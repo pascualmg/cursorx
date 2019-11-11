1660987c51b4d16b79e468b66670d6c56b43cadb
pascualmg <pascual.munoz@pccomponentes.com>
2019-11-11 15:42:49 +0100

    interval take mapTo

--- src/create.js
+++ src/create.js
@@ -1,9 +1,14 @@
 import { displayLog } from './utils';
+import { interval } from 'rxjs'
+import { mapTo, take } from 'rxjs/operators'
 
 export default () => {
     /** start coding */
 
-
+    interval(1000,2).pipe(
+      take(2),
+      mapTo('hola')
+    ).subscribe(displayLog)
 
     /** end coding */
 }
